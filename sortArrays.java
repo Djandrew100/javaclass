7f934925772d63dd96fd2734be8707c71e3b679f
Djandrew100 <120802370+Djandrew100@users.noreply.github.com>
2022-12-20 20:46:39 -0600

    new code block

--- sortArrays.java
+++ sortArrays.java
@@ -15,6 +15,11 @@ public class SortArrays {
 		
 		Arrays.sort(names);
 		System.out.println(Arrays.toString(names));
+               
+                System.out.println("person 2 added new line");
+                System.out.println("Array is not sorting");
+
+
 		
 	}
 }
